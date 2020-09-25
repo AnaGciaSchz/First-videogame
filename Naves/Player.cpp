68500181f380c1b2c302375955555480f352a8f4
AnaGciaSchz <anags.clm@gmail.com>
2020-09-25 11:45:09 +0200

    Diferenciamos animaciones infinitas de finitas

--- Naves/Player.cpp
+++ Naves/Player.cpp
@@ -3,13 +3,13 @@
 Player::Player(float x, float y, Game* game) :Actor("res/jugador.png",x,y,35,35,game){
 	orientation = game->orientationRight;
 	aIdleRight = new Animation("res/jugador_idle_derecha.png", width, height,
-		320, 40, 6, 8, game);
+		320, 40, 6, 8,true, game);
 	aIdleLeft = new Animation("res/jugador_idle_izquierda.png", width, height,
-		320, 40, 6, 8, game);
+		320, 40, 6, 8,true, game);
 	aRunningRight = new Animation("res/jugador_corriendo_derecha.png", width, height,
-		320, 40, 6, 8, game);
+		320, 40, 6, 8,true, game);
 	aRunningLeft = new Animation("res/jugador_corriendo_izquierda.png", width, height,
-		320, 40, 6, 8, game);
+		320, 40, 6, 8,true, game);
 	animation = aIdleRight;
 
 }
