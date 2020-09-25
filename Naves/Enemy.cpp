ebd3038ffefcb34d9ddccd6a0d3f495e8dbb91f5
AnaGciaSchz <anags.clm@gmail.com>
2020-09-25 10:42:01 +0200

    El enemigo ejecuta una animación

--- Naves/Enemy.cpp
+++ Naves/Enemy.cpp
@@ -6,10 +6,21 @@ Enemy::Enemy(float x, float y, Game* game)
 	: Actor("res/enemigo.png", x, y, 36, 40, game) {
 
 	vx = -1;
+
+	aMoving = new Animation("res/enemigo_movimiento.png", width, height,
+		108, 40, 6, 3, game);
+	animation = aMoving;
+
 }
 
 void Enemy::update() {
-	x = x + vx;
+	// Actualizar la animación
+	animation->update();
 
+	x = x + vx;
+}
+
+void Enemy::draw() {
+	animation->draw(x, y);
 }
 
--- Naves/Enemy.h
+++ Naves/Enemy.h
@@ -1,10 +1,15 @@
 #pragma once
 #include "Actor.h"
+#include "Animation.h" 
 
 class Enemy : public Actor
 {
 public:
 	Enemy(float x, float y, Game* game);
 	void update();
+	void draw() override; // Va a sobrescribir
+	Animation* aMoving;
+	Animation* animation; // Referencia a la animación mostrada
+
 };
 
